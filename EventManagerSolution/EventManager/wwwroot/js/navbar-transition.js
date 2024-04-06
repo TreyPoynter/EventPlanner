b08025f8a27a5f39c1e5e41c1fca0e22e4ff1b2e
TreyPoynter <treypoynter@gmail.com>
2024-04-06 09:33:33 -0500

    Modified Navbar

---
+++ EventManagerSolution/EventManager/wwwroot/js/navbar-transition.js
@@ -0,0 +1,12 @@
+﻿window.addEventListener('scroll', function () {
+    const jumbotron = document.getElementById('jumbotron');
+    const navbar = document.getElementById('navbar');
+
+    if (window.scrollY > 0) { // Change transparency as soon as user starts scrolling
+        navbar.classList.remove('transparent');
+        navbar.classList.add('white-bg');
+    } else {
+        navbar.classList.remove('white-bg');
+        navbar.classList.add('transparent');
+    }
+});
