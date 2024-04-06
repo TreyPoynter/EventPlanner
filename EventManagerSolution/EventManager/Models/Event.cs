70e5ea6c01ff02927dbc2e7cfe58061fab620a77
TreyPoynter <treypoynter@gmail.com>
2024-04-05 20:21:03 -0500

    Added Event Props

--- EventManagerSolution/EventManager/Models/Event.cs
+++ EventManagerSolution/EventManager/Models/Event.cs
@@ -6,6 +6,10 @@ namespace EventManager.Models
     {
         public int EventId { get; set; }
         public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public string? Address { get; set; }
 
         public int TypeId { get; set; }
         [ForeignKey(nameof(TypeId))]
