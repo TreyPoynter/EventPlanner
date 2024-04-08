15cd4295eb32cb17559618dcc7b96b17cc0543cb
TreyPoynter <treypoynter@gmail.com>
2024-04-08 15:18:50 -0500

    Update EventController.cs
    added repo variables

--- EventManagerSolution/EventManager/Controllers/EventController.cs
+++ EventManagerSolution/EventManager/Controllers/EventController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EventManager.Data;
+using EventManager.Models;
+using EventManager.Models.DataLayer;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace EventManager.Controllers
 {
     public class EventController : Controller
     {
+        private readonly Repository<EventType> typesDb;
+        private readonly Repository<UserEventInterest> interestsDb;
+        private readonly Repository<Event> eventsDb;
+
+        public EventController(AppDbContext ctx)
+        {
+            typesDb = new Repository<EventType>(ctx);
+            interestsDb = new Repository<UserEventInterest>(ctx);
+            eventsDb = new Repository<Event>(ctx);
+        }
+
         [Route("/Events")]
         public IActionResult Index()
         {
@@ -11,7 +26,22 @@ namespace EventManager.Controllers
         }
         public IActionResult Create()
         {
+            ViewBag.EventTypes = typesDb.List(new QueryOptions<EventType>());
+
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Event @event)
+        {
+            @event.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(@event);
         }
     }
 }
