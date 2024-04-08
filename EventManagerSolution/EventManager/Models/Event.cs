3e57f8173a1288f0369adb0fbffb654cdad0f0ef
TreyPoynter <treypoynter@gmail.com>
2024-04-08 15:19:23 -0500

    Update Event.cs
    Attributes

--- EventManagerSolution/EventManager/Models/Event.cs
+++ EventManagerSolution/EventManager/Models/Event.cs
@@ -1,21 +1,40 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EventManager.Models
 {
     public class Event
     {
         public int EventId { get; set; }
+        [Required]
         public string? Name { get; set; }
+        [Required]
         public string? Description { get; set; }
+        [Required]
         public DateTime? StartTime { get; set; }
+        [Required]
         public DateTime? EndTime { get; set; }
+        [Required]
         public string? Address { get; set; }
+        [Required]
         public bool IsEveryoneInvited { get; set; }
+        [Required]
         public string? EventBanner { get; set; }
+        [Required]
         public string? EventIcon { get; set; }
 
+        [Required]
         public int TypeId { get; set; }
         [ForeignKey(nameof(TypeId))]
+        [ValidateNever]
+        [NotMapped]
         public EventType Type { get; set; } = null!;
+
+        public string? UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        [ValidateNever]
+        [NotMapped]
+        public ApplicationUser User { get; set; }
     }
 }
