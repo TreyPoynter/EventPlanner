c755a23359e282f367339283308df1f274314c40
TreyPoynter <treypoynter@gmail.com>
2024-04-10 12:51:54 -0500

    Create EventRepository.cs

---
+++ EventManagerSolution/EventManager/Models/DataLayer/EventRepository.cs
@@ -0,0 +1,21 @@
+﻿using EventManager.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManager.Models.DataLayer
+{
+    public class EventRepository<T> : Repository<T> where T : class
+    {
+        private readonly DbSet<Event> _events;
+
+        public EventRepository(AppDbContext ctx) : base(ctx)
+        {
+            _events = ctx.Set<Event>();
+        }
+
+        public Event? GetEventById(int id)
+        {
+            return _events.Include(e => e.User).Include(e => e.Type)
+                .FirstOrDefault(e => e.EventId == id);
+        }
+    }
+}
