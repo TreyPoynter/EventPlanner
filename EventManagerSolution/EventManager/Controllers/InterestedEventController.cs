7dff6c4a10335be95864177a9079d36e3f488c07
TreyPoynter <treypoynter@gmail.com>
2024-04-11 11:11:20 -0500

    Create InterestedEventController.cs

---
+++ EventManagerSolution/EventManager/Controllers/InterestedEventController.cs
@@ -0,0 +1,23 @@
+﻿using EventManager.Data;
+using EventManager.Models;
+using EventManager.Models.DataLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManager.Controllers
+{
+    public class InterestedEventController : Controller
+    {
+        Repository<UserEventInterest> _userInterestLevelDb;
+
+        public InterestedEventController(AppDbContext ctx)
+        {
+            _userInterestLevelDb = new Repository<UserEventInterest>(ctx);
+        }
+
+        [HttpPost]
+        public IActionResult AddInterest(InterestLevel interestLevel)
+        {
+            return View();
+        }
+    }
+}
