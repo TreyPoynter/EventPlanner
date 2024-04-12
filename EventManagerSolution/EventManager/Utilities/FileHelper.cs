832f353c9dd07cc62996a03d673181191bc5e473
TreyPoynter <treypoynter@gmail.com>
2024-04-12 08:48:45 -0500

    Create FileHelper.cs

---
+++ EventManagerSolution/EventManager/Utilities/FileHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace EventManager.Utilities
+{
+    public static class FileHelper
+    {
+        public static async Task<string> UploadFileAsync(IFormFile file, string directory, 
+            IWebHostEnvironment hostEnvironment)
+        {
+            string uploadDir = Path.Combine(hostEnvironment.WebRootPath, directory);
+            string fileName = Path.GetFileName(file.FileName);
+            string filePath = Path.Combine(uploadDir, fileName);
+
+            // Ensure directory exists
+            Directory.CreateDirectory(uploadDir);
+
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+
+            return filePath;
+        }
+    }
+}
